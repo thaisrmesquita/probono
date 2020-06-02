a92cb73c7471a09878a1bc64bedf5385e33ec691
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2020-06-02 14:57:01 -0300

    Configuração inicial do frontend

--- backend/src/controllers/TranslationController.js
+++ backend/src/controllers/TranslationController.js
@@ -1,7 +1,25 @@
+import Translation from "../models/Translation";
+
 class TranslationController {
 
     async store(req, res) {
+        const { rule, description } = req. body;
+        
+        let translate = Translation.create({
+            rule,
+            description
+        });
+        
 
+        return res.json(translate);
     }
+
+    delete
+
+    update
+
+    listar
+    
+    
 }
 export default TranslationController;

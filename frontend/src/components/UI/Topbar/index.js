9501350676b21bc8023805423981af84d1ccf0da
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2020-06-07 23:34:59 -0300

    Adicionado metodo de logout da aplicação

--- frontend/src/components/UI/Topbar/index.js
+++ frontend/src/components/UI/Topbar/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import  {useHistory} from 'react-router-dom';
 import Logo from '../../Assets/images/logo.png';
 import { FiLogOut } from 'react-icons/fi';
 
 import './topbar.css';
 
 function Topbar () {
+    const history = useHistory();
+    function handleLogout () {
+        localStorage.clear();
+        history.push('/');
+
+    }
     return(
         <div className="topbar">
             <div className="topbar-container">
@@ -12,7 +19,7 @@ function Topbar () {
                     <img className="logo" src={Logo}/>
                 </div>
                 <div className="topbar-icon-logout">
-                    <FiLogOut size={24} color="#fff" />
+                    <FiLogOut size={24} color="#fff" onClick={handleLogout} />
                 </div>
             </div>
         </div>
